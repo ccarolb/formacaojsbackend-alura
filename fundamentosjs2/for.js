73b849469db8df6b0cdeb3f0209140188178089d
Carolina Borges <carolinaborgescardoso@gmail.com>
2021-11-07 18:14:37 -0300

    praticando for

---
+++ fundamentosjs2/for.js
@@ -0,0 +1,8 @@
+//Exercício: For
+//Imprima o índice e a lista com os seguintes números: 100, 200, 300, 400, 500, 600.
+
+const numeros = [100, 200, 300, 400, 500, 600]
+
+for (let i = 0; i < numeros.length; i++) {
+    console.log(`Para o índice ${i}, o número correspondente é ${numeros[i]}`)
+}
