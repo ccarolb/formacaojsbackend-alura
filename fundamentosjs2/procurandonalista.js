1debfba0917e7db476345aff08a7cc9919056996
Carolina Borges <carolinaborgescardoso@gmail.com>
2021-11-07 17:58:09 -0300

    utilizando os métodos includes e indexOf para buscar informações numa matriz

---
+++ fundamentosjs2/procurandonalista.js
@@ -0,0 +1,20 @@
+//Exercício 2: Procurando na lista
+//a) Crie uma função que recebe como argumento o nome de um aluno
+//b) Verifique se o aluno está presente na lista e retorne a média final que se encontra no mesmo índice.
+//c) Caso o nome do aluno não esteja na lista, retorne uma mensagem indicando que o aluno não foi encontrado.
+
+let nomes = ["João", "Juliana", "Caio", "Ana"]
+let notas = [10, 8, 7.5, 9]
+let nomeComNota = [nomes, notas]
+
+function buscaAluno(aluno) {
+
+    if (nomeComNota[0].includes(aluno)) {
+        indice = nomeComNota[0].indexOf(aluno)
+        return `${nomeComNota[0][indice]}, sua nota é ${nomeComNota[1][indice]}.`
+    } else {
+        return "O aluno não foi encontrado."
+    }
+}
+
+console.log(buscaAluno("João"))
