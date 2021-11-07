1c2b5d585137237889a08d77c34e9a4739f65be1
Carolina Borges <carolinaborgescardoso@gmail.com>
2021-11-07 16:06:32 -0300

    concatenando arrays com concat()

---
+++ fundamentosjs2/concat.js
@@ -0,0 +1,8 @@
+//Exercício: juntando as salas
+//Haverá uma palestra sobre padrões de projetos para as alunas da sala de JavaScript e Python. Junte ambas as salas em uma única lista que exiba todas as alunas.
+
+let turmaJs = ["Carolina", "Fernanda", "Azaléia", "Cora", "Paula"]
+let turmaPython = ["Juliane", "Nina", "Sara", "Beatriz", "Sol"]
+
+let turmaUnificada = turmaJs.concat(turmaPython)
+console.log(turmaUnificada)
