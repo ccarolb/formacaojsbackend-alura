af20be57b8d5cb2e696f17ea2fac9b9915eda6f1
Carolina Borges <carolinaborgescardoso@gmail.com>
2021-11-07 20:21:33 -0300

    padronizando nomes com map e toUpperCase

--- fundamentosjs2/map.js
+++ fundamentosjs2/map.js
@@ -5,3 +5,11 @@ let notas = [10, 9, 8, 7, 6]
 let notaCorreta = notas.map((nota) => nota === 10 ? nota : ++nota)
 
 console.log(notaCorreta)
+
+//Exercício: padronizando os nomes
+//Precisamos padronizar a lista de alunos para conter apenas letras maiúsculas: ["ana Julia", "Caio vinicius", "BIA silva"]
+
+let alunos = ["ana Julia", "Caio vinicius", "BIA silva"]
+
+let listaAlunos = alunos.map(aluno => aluno.toUpperCase())
+console.log(listaAlunos)
