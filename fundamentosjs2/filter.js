204c947f2349c856fb0e396db32eedb3bb686a5b
Carolina Borges <carolinaborgescardoso@gmail.com>
2021-11-07 20:48:06 -0300

    trabalhando com filter

---
+++ fundamentosjs2/filter.js
@@ -0,0 +1,15 @@
+//Exercício: filtrando por nota
+//Depois de calcular a média dos alunos, precisamos mostrar quem está reprovado entre os alunos "Ana", "Marcos", "Maria", "Mauro", com as respectivas notas: [7, 4.5, 8, 7.5].
+
+let nomeNota = [
+    { nome: "Ana", nota: 7 },
+    { nome: "Marcos", nota: 4.5 },
+    { nome: "Maria", nota: 8 },
+    { nome: "Mauro", nota: 7.5 }
+]
+
+function verificaAprovacao() {
+    return nomeNota.filter(aluno => aluno.nota >= 7 ? console.log(`${aluno.nome} está aprovado.`) : console.log(`${aluno.nome} está reprovado.`))
+}
+
+verificaAprovacao()
