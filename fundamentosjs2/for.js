2188d4ea2f91c7ff50d929d966c1a68a5cf77018
Carolina Borges <carolinaborgescardoso@gmail.com>
2021-11-07 18:25:51 -0300

    calculando média com for

--- fundamentosjs2/for.js
+++ fundamentosjs2/for.js
@@ -6,3 +6,19 @@ const numeros = [100, 200, 300, 400, 500, 600]
 for (let i = 0; i < numeros.length; i++) {
     console.log(`Para o índice ${i}, o número correspondente é ${numeros[i]}`)
 }
+
+//Exercício: Média com for
+//Calcule a média de um array de notas utilizando o for.
+
+function mediaNotas() {
+    let notas = [6, 7.5, 8, 10, 9]
+    let somaNotas = 0;
+
+    for (let i = 0; i < notas.length; i++) {
+        somaNotas += notas[i]
+    }
+
+    return somaNotas / notas.length
+}
+
+console.log(mediaNotas())
