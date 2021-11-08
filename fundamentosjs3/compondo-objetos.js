27580bdd99abf4efcaefd2923a06cd7c38c09525
Carolina Borges <carolinaborgescardoso@gmail.com>
2021-11-08 19:55:04 -0300

    lista de objetos

--- fundamentosjs3/compondo-objetos.js
+++ fundamentosjs3/compondo-objetos.js
@@ -11,17 +11,17 @@ const cliente = {
 cliente.dependentes = [{
     nome: "Nina",
     idade: 2,
-    espécie: "felina"
+    espécie: "felina",
 },
 {
     nome: "Betina",
     idade: 2,
-    espécie: "felina"
+    espécie: "felina",
 },
 {
     nome: "Kiki",
     idade: 1,
-    espécie: "felina"
+    espécie: "felina",
 }]
 
 console.log(cliente)
@@ -29,3 +29,17 @@ console.log(cliente)
 //Alterando o valor da propriedade
 cliente.dependentes[0].idade = 2.5
 console.log(cliente)
+
+//Exercício: lista de dependentes.
+//Verificar a melhor forma de agregar a informação de um novo dependente ao objeto cliente.
+
+//Adicionando mais um dependente com o método de array push():
+cliente.dependentes.push({
+    nome: "Kiko",
+    idade: 8,
+    espécie: "canina",
+})
+console.log(cliente)
+
+let dependenteMaisNovo = cliente.dependentes.filter(dependente => dependente.idade === 1)
+console.log(dependenteMaisNovo[0].nome)
