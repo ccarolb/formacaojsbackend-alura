fa47ec75349f9fc959f2cdf967bd9d663c8b6f6e
Carolina Borges <carolinaborgescardoso@gmail.com>
2021-11-22 20:10:53 -0300

    protótipo de cliente utilizando classe

---
+++ fundamentosjs3/cliente-classe.js
@@ -0,0 +1,23 @@
+//Criar um protótipo de cliente utilizando classes
+
+class Cliente {
+    constructor(nome, idade, cpf, email, conta, saldo) {
+        this.nome = nome,
+            this.idade = idade,
+            this.cpf = cpf,
+            this.email = email,
+            this.conta = conta,
+            this.saldo = saldo
+    }
+
+    depositar(valor) {
+        this.saldo += valor
+    }
+
+}
+
+const cliente = new Cliente("Fulano", 21, "23123214435", "fulano@email.com", "23674673467", 0)
+
+cliente.depositar(50)
+
+console.log(cliente)
